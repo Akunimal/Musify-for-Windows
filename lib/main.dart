03817ec8bcdd84bb31eb00fe059a534583277c5e
Akunimal <inti_gesell@hotmail.com>
2026-07-23 09:53:07 -0300

    fix: restore media_kit (freeze was pre-existing, not caused by video mode)

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:media_kit/media_kit.dart';
 import 'package:musify/extensions/l10n.dart';
 import 'package:musify/localization/app_localizations.dart';
 import 'package:musify/services/audio_service.dart';
@@ -292,6 +293,7 @@ void main() async {
 
 Future<void> initialisation() async {
   try {
+    MediaKit.ensureInitialized();
     await Hive.initFlutter();
 
     await Future.wait([
